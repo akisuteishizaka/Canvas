5cfd5da7d9432af6c3367b9628b27d1dd8ac8c89
taiti <taiti@stone-rise.com>
2015-05-19 18:08:12 +0900

    chapter11

--- js/myscript.js
+++ js/myscript.js
@@ -8,4 +8,12 @@ function draw() {
 	var canvas = document.getElementById('mycanvas');
 	if (!canvas || !canvas.getContext) return false;
 	var ctx = canvas.getContext('2d');
+
+	ctx.font = 'bold 20px Verdana';
+	ctx.textAlign = 'left';
+	ctx.fillStyle = 'red';
+	ctx.strokeStyle = 'red';
+
+	ctx.fillText('taitiakisute',20, 20, 200);
+	ctx.strokeText('taitiate',20, 60, 200)
 }

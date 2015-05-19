b713a00a1ad77af27a7bd781762d4bf25487bcdc
taiti <taiti@stone-rise.com>
2015-05-19 11:33:26 +0900

    master added

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,11 @@
 // myscript.js
+
+window.onload = function() {
+	draw();
+}
+
+function draw() {
+	var canvas = document.getElementById('mycanvas');
+	if (!canvas || !canvas.getContext) return false;
+	var ctx = canvas.getContext('2d');
+}

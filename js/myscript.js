78d5411863b17770214c7d24de8c095eb312545f
taiti <taiti@stone-rise.com>
2015-05-19 18:19:49 +0900

    chapter13

--- js/myscript.js
+++ js/myscript.js
@@ -8,4 +8,12 @@ function draw() {
 	var canvas = document.getElementById('mycanvas');
 	if (!canvas || !canvas.getContext) return false;
 	var ctx = canvas.getContext('2d');
+
+	var img = new Image();
+	img.src = 'img/pratica_form_submit.png';
+	img.onload = function() {
+		var pattern = ctx.createPattern(img, 'repeat');
+		ctx.fillStyle = pattern;
+		ctx.fillRect(20, 20, 100, 100);
+	}
 }
